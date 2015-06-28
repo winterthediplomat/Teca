ecc109150640849860eb26c17440bbbfe3cdc676
alfateam123 <alfateam123@gmail.com>
2015-06-28 12:34:20 +0200

    Add setup, prepare tests

---
+++ setup.py
@@ -0,0 +1,27 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='Teca',
+    version='0.1.9.1',
+    author='alfateam123',
+    author_email='alfateam123@gmail.com',
+    packages=['teca'],
+    #scripts=['bin/index', 'bin/post_details', 'bin/thread_details', 'bin/search'],
+    url='http://github.com/alfateam123/Teca',
+    #license='LICENSE.txt',
+    description='simple static gallery generator',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "jinja2",
+        "beautifulsoup4"
+    ],
+    classifiers=[
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    test_suite="tests"
+)
