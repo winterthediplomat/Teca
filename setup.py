2a3ef58e11a2dc17af8a255738b3565c783178ae
alfateam123 <alfateam123@hotmail.it>
2015-07-21 08:52:01 -0400

    pep8 on setup.py

--- setup.py
+++ setup.py
@@ -3,29 +3,29 @@ from setuptools import setup
 
 setup(
     name='Teca',
-    version='0.1.10',
+    version='0.1.11',
     author='alfateam123',
     author_email='alfateam123@gmail.com',
     packages=['teca'],
     #scripts=['bin/index', 'bin/post_details', 'bin/thread_details', 'bin/search'],
     url='http://github.com/alfateam123/Teca',
-    #license='LICENSE.txt',
+    # license='LICENSE.txt',
     description='simple static gallery generator',
     long_description=open('README.md').read(),
     install_requires=[
-        "jinja2", 
+        "jinja2",
         "pillow",
         "six"
     ],
     extras_require={
-      "test": ["nose", "coverage"]
+        "test": ["nose", "mockfs", "coverage"]
     },
     classifiers=[
-    "Development Status :: 4 - Beta",
-    "Environment :: Web Environment",
-    "Intended Audience :: Developers",
-    "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
-    "Topic :: Software Development :: Libraries :: Python Modules"
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     test_suite="tests"
 )
